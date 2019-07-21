2cfbef7812990ff3b8e5c6de03bfa9fd25c33e8d
zefie <zefie@zefie.net>
2019-07-21 04:43:51 -0400

    Add UAC Class

---
+++ ZefieLib/UAC.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZefieLib
+{
+    public class UAC
+    {
+        /// <summary>
+        /// Returns true if process has Administrative Access
+        /// </summary>
+        public static bool IsAdmin
+        {
+            get
+            {
+                WindowsIdentity identity = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        /// <summary>
+        /// Attempts to relaunch the current executable with Administrative Access
+        /// </summary>
+        /// <param name="args">CLI args to pass to ourselves</param>
+        /// <returns>true if successful, so you can properly know to terminate your non-admin copy</returns>
+        public static bool RunAsAdministrator(string args)
+        {
+            string executable = Process.GetCurrentProcess().MainModule.FileName;
+            return RunAsAdministrator(executable, args);
+        }
+
+        /// <summary>
+        /// Attempts to launch an executable with Administrative Access
+        /// </summary>
+        /// <param name="executable">full path to executable</param>
+        /// <param name="args">arguments to pass to executable</param>
+        /// <returns></returns>
+        public static bool RunAsAdministrator(string executable, string args)
+        {
+            if (IsAdmin == false)
+            {
+                try
+                {
+                    ProcessStartInfo startInfo = new ProcessStartInfo(executable)
+                    {
+                        Arguments = args,
+                        Verb = "runas"
+                    };
+                    Process.Start(startInfo);
+                }
+                catch
+                {
+                    DialogResult errormsg = MessageBox.Show("There was an error gaining administrative privileges", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                    if (errormsg == DialogResult.Retry)
+                    {
+                        RunAsAdministrator(executable, args);
+                    }
+                }
+                return false;
+            }
+            return false;
+        }
+    }
+}
